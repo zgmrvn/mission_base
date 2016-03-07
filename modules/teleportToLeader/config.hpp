5c788444842955a3866ebd02b45723806edbb131
zgmrvn <me@zgmrvn.com>
2016-03-07 12:50:55 +0100

    correction commentaires

--- modules/teleportToLeader/config.hpp
+++ modules/teleportToLeader/config.hpp
@@ -2,7 +2,7 @@ class TeleportToLeader {
 	// activer ou désactiver le module
 	enabled = 1;
 
-	// liste des nom des drapeaux créés par le module "ActionsFlags" sur lesquels il faut ajouter l'action
+	// liste des noms des drapeaux créés par le module "ActionsFlags" sur lesquels il faut ajouter l'action
 	flags[] = {
 		/*
 		"base",
