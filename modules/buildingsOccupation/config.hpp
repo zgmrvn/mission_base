66cd469d780d282544dbe34ad00303a0c6976e5c
zgmrvn <me@zgmrvn.com>
2016-07-14 21:34:36 +0200

    ajouts bâtiments Tanoa

--- modules/buildingsOccupation/config.hpp
+++ modules/buildingsOccupation/config.hpp
@@ -23,10 +23,13 @@ class BuildingsOccupation {
 
 	// bâtiments et positions d'occupation
 	class Buildings {
-		// bâtiments Arma 3, vanilla
+		// bâtiments Arma 3 vanilla, Stratis, Altis
 		#include "inc\vanilla.hpp"
 
 		// bâtiments portés d'Arma 2
 		#include "inc\arma2.hpp"
+
+		// bâtiments de Tanoa
+		#include "inc\tanoa.hpp"
 	};
 };
