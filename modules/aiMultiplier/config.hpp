99082f16a1e782f8dd5fb809c3cc8fc65b9e38a1
zgmrvn <me@zgmrvn.com>
2016-11-02 00:11:06 +0100

    ajout du module aiMultiplier

---
+++ modules/aiMultiplier/config.hpp
@@ -0,0 +1,7 @@
+/*
+	framework de mission du CORP
+	http://www.corp-arma.fr
+*/
+
+// activer ou désactiver le module
+enabled = 1;
---
+++ modules/aiMultiplier/content/wrapper.hpp
@@ -0,0 +1,8 @@
+/*
+	framework de mission du CORP
+	http://www.corp-arma.fr
+*/
+
+class aiMultiplier {
+	#include "..\config.hpp"
+};
