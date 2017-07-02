4e401bee62e080be49000e7da2ceee07b2fc1863
zgmrvn <zgmrvn+github@gmail.com>
2017-07-02 13:45:04 +0200

    suppression du module iaMultiplier

--- modules/aiMultiplier/config.hpp
+++
@@ -1,7 +0,0 @@
-/*
-	framework de mission du CORP
-	http://www.corp-arma.fr
-*/
-
-// activer ou désactiver le module
-enabled = 1;
--- modules/aiMultiplier/content/wrapper.hpp
+++
@@ -1,8 +0,0 @@
-/*
-	framework de mission du CORP
-	http://www.corp-arma.fr
-*/
-
-class aiMultiplier {
-	#include "..\config.hpp"
-};
