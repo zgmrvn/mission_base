f03ad85a5a84c542f19c8583c5393771131e6b6c
zgmrvn <zgmrvn+github@gmail.com>
2017-07-04 14:20:09 +0200

    missionBriefing - gestion du multi-paragraphe

--- modules/missionBriefing/config.hpp
+++ modules/missionBriefing/config.hpp
@@ -9,12 +9,17 @@ enabled = 1;
 // liste des "diaries" dans l'onglet briefing
 class Diaries {
 	class Objerctif1 {
-		title	= "Saboter l'antenne relais";
-		content	= "Sabotez l'<marker name='antenna'>antenne relais</marker> pour empêcher les <marker name='renforcementNorth'>renforts au Nord</marker> d'être prévenus.";
+		title		 = "Saboter l'antenne relais";
+		paragraphs[] = {
+			"Sabotez l'<marker name='antenna'>antenne relais</marker> pour empêcher les <marker name='renforcementNorth'>renforts au Nord</marker> d'être prévenus.",
+			"Un 2e paragraphe, gestion des paragraphes pour simplifier la rédaction de briefing."
+		};
 	};
 
 	class Test2 {
-		title	= "Éliminer les scientifiques";
-		content	= "Éliminez les scientifiques qui travaillent sur l'enrichissement d'uranium.";
+		title		 = "Éliminer les scientifiques";
+		paragraphs[] = {
+			"Éliminez les scientifiques qui travaillent sur l'enrichissement d'uranium."
+		};
 	};
 };
--- modules/missionBriefing/content/initScriptClient.sqf
+++ modules/missionBriefing/content/initScriptClient.sqf
@@ -8,10 +8,21 @@ if ((getNumber (missionConfigFile >> "MissionBriefing" >> "enabled")) == 1) then
 	private _diaries = (missionConfigFile >> "MissionBriefing" >> "Diaries") call BIS_fnc_getCfgSubClasses;
 
 	for [{private _i = (count _diaries) - 1}, {_i >= 0}, {_i = _i - 1}] do {
+		private _paragraphs	= getArray (missionConfigFile >> "MissionBriefing" >> "Diaries" >> (_diaries select _i) >> "paragraphs");
+		private _content	= "";
+
+		// concaténation des paragraphes
+		{
+			private _separator = ["<br/><br/>", ""] select (_forEachIndex == 0);
+
+			_content = [_content, _x] joinString _separator;
+		} forEach _paragraphs;
+
+		// création de l'entée
 		player createDiaryRecord [
 			"Diary", [
 				getText (missionConfigFile >> "MissionBriefing" >> "Diaries" >> (_diaries select _i) >> "title"),
-				getText (missionConfigFile >> "MissionBriefing" >> "Diaries" >> (_diaries select _i) >> "content")
+				_content
 			]
 		];
 	};
